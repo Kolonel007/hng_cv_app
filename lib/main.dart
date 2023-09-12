dfb89ecca8ecb29f9c9f44718fd3bfd0cc2cfe44
kolade <kolademodupe007@gmail.com>
2023-09-12 12:03:26 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Text("Profile", style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
               Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", style: TextStyle(fontSize: 10.0),),
               ]),
-          ),
+          ),``
       
         ]),
       ),
